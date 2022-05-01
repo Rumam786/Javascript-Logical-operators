4137d0b39c66aae1a9c514afe9902e72bd3b800d
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-05-02 01:48:36 +0500

    Add files via upload

---
+++ family.js
@@ -0,0 +1,20 @@
+// And
+var x = 20;
+var a = 22;
+if(a > x && x < a)
+{document.write("You Are Eligible");
+};
+document.write("<br>");
+// OR
+var c = 19;
+var d = 15;
+if(c > d || d > c)
+{document.write("i am Eligible");
+};
+document.write("<br>");
+// Not
+var b = 23;
+var z = 11;
+if(!b < z)
+{document.write("True");
+};
